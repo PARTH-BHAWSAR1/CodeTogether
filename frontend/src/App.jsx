6de54f67a458e6ff7c41c360e341ca08c4270b99
Parth Bhawsar <119779448+PARTH-BHAWSAR1@users.noreply.github.com>
2025-08-09 22:45:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 import Editor from "@monaco-editor/react";
 
 
-const socket = io("http://localhost:5000");
+const socket = io();
 
 const App = () => {
   const [joined, setJoined] = useState(false);
